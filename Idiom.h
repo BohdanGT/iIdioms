834b3e434cfe8990a0381f2b210802634aa03247
Bohdan Kachur <bohdan.kachur@Live.com>
2011-12-03 22:59:17 +0200

    Added Idiom class

---
+++ Idiom.h
@@ -0,0 +1,16 @@
+//
+//  Idiom.h
+//  iIdioms
+//
+//  Created by Bohdan Kachur on 12/3/11.
+//  Copyright (c) 2011 bbsoft. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Idiom : NSObject
+
+@property (copy, nonatomic) NSString *IdiomName;
+@property (copy, nonatomic) NSString *IdiomDescription;
+@property (copy, nonatomic) NSString *IdiomExample;
+@end
---
+++ Idiom.m
@@ -0,0 +1,15 @@
+//
+//  Idiom.m
+//  iIdioms
+//
+//  Created by Bohdan Kachur on 12/3/11.
+//  Copyright (c) 2011 bbsoft. All rights reserved.
+//
+
+#import "Idiom.h"
+
+@implementation Idiom
+@synthesize IdiomName = _idiomName;
+@synthesize IdiomDescription = _idiomDescription;
+@synthesize IdiomExample = _IdiomExample;
+@end
